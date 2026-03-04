797de6d445d21aeb26ff334fca05499f54c574fa
GARVIT <garvitbhattpc@gmail.com>
2026-03-04 14:46:01 +0530

    fix: .env.example mongoDB url fix

--- Learning_Garvit/colabs/src/app/page.tsx
+++ Learning_Garvit/colabs/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import Link from 'next/link'
 
 const page = () => {
   return (
     <div className='flex min-h-screen items-center justify-center'>
-      hello
+      click  <Link href='/documents/1234'><span className='text-blue-500 underline'>&nbsp; here &nbsp;</span></Link> to go to documents
+      
     </div>
   )
 }
