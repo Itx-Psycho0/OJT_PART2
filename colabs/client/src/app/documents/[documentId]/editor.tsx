bc079b099114773b1892976548ea3c52536fe442
GARVIT <garvitbhattpc@gmail.com>
2026-03-25 08:37:10 +0530

    feat(editor): integrate TipTap extensions to support core editing features

--- colabs/client/src/app/documents/[documentId]/editor.tsx
+++ colabs/client/src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { TaskItem} from '@tiptap/extension-task-item'
+import {TaskList} from  '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from "tiptap-extension-resize-image";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,8 +19,35 @@ export const Editor = () => {
         },
 
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      ImageResize,
+      Image,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      TaskItem.configure({
+        nested:true,
+      }),
+      TaskList
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     
     
     immediatelyRender: false,
