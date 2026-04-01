6ed271f1adc742bd4984a3b864db9fec3a7a1146
GARVIT <garvitbhattpc@gmail.com>
2026-04-01 18:42:25 +0530

    feat: add menubar component with basic UI structure (no functionality yet)

---
+++ colabs/client/src/app/documents/[documentId]/document-input.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsCloudCheck} from 'react-icons/bs'
+const DocumentInput = () => {
+  return (
+    <div className='flex items-center gap-2'>
+        <span className='text-lg cursor-pointer px-1.5 truncate'>Untitled Document</span>
+        <BsCloudCheck/>
+      
+    </div>
+  )
+}
+
+export default DocumentInput
