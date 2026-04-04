4a63d8ea2f0968822ebd260fdf5bc2043dc8d2d9
GARVIT <garvitbhattpc@gmail.com>
2026-04-04 12:29:33 +0530

    feat(navbar): add navbar with nuqs-powered search component
    - Added Navbar component with integrated Search input
    - Used nuqs to sync search query with URL search params
    - Search state is reflected and updated directly in the URL

---
+++ colabs/client/src/app/(home)/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+import SearchInput from "./search-input";
+
+export const Navbar=()=>{
+    return (
+        <nav className="flex items-center mt-4 justify-between h-full w-full ">
+            <div className="flex gap-3 items-center shrink-0 pr-6">
+                <Link href="/">
+                <Image src='/logo2.svg' alt="logo" width={180} height={180} /> 
+                </Link>
+                
+            </div>
+            <SearchInput/>
+            <div/>
+        </nav>
+
+    )
+}
--- colabs/client/src/app/(home)/page.tsx
+++ colabs/client/src/app/(home)/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
-
+import { Navbar } from './navbar'
 const page = () => {
   return (
-    <div className='flex min-h-screen items-center justify-center'>
+    <div className='flex min-h-screen flex-col '>
+      <div className='fixed top-0 left-0 right-0 z-10 h-16  bg-white p-4'>
+        <Navbar/>
+      </div>
+      <div>
       click  <Link href='/documents/1234'><span className='text-blue-500 underline'>&nbsp; here &nbsp;</span></Link> to go to documents
-      
+      </div>
     </div>
   )
 }
