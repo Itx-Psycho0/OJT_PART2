c7a4d90f9794b336f8a1ade55fc7aac0641b3fdd
GARVIT <garvitbhattpc@gmail.com>
2026-04-04 12:39:26 +0530

    fix: wrap Navbar in Suspense boundary to resolve useSearchParams SSR error

--- colabs/client/src/app/(home)/page.tsx
+++ colabs/client/src/app/(home)/page.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Link from 'next/link'
 import { Navbar } from './navbar'
-const page = () => {
+
+const Page = () => {
   return (
-    <div className='flex min-h-screen flex-col '>
-      <div className='fixed top-0 left-0 right-0 z-10 h-16  bg-white p-4'>
-        <Navbar/>
+    <div className='flex min-h-screen flex-col'>
+      <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4'>
+        <Suspense fallback={null}>
+          <Navbar />
+        </Suspense>
       </div>
       <div>
-      click  <Link href='/documents/1234'><span className='text-blue-500 underline'>&nbsp; here &nbsp;</span></Link> to go to documents
+        click <Link href='/documents/1234'><span className='text-blue-500 underline'>&nbsp; here &nbsp;</span></Link> to go to documents
       </div>
     </div>
   )
 }
 
-export default page
+export default Page
